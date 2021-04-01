ffc95ada49b56eacda1308f9f55114b077a846cc
miloszKubiak <milypan69@gmail.com>
2021-04-01 19:07:26 +0200

    Added a SetTime function

--- main.js
+++ main.js
@@ -17,11 +17,28 @@ const saveBtn = document.querySelector('.save');
 const eventSpan = document.querySelector('.event')
 let usersTime;
 
+const setTime = () => {
+    const currentTime = new Date();
+    const result = usersTime - currentTime;
+    const days = Math.floor(result / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(result / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(result / 1000 / 60) % 60;
+    const seconds = Math.floor(result / 1000) % 60;
+
+    daysCount.textContent = days;
+    hoursCount.textContent = hours;
+    minutesCount.textContent = minutes;
+    secondsCount.textContent = seconds;
+}
+
 const appUpdate = () => {
     eventSpan.textContent = eventName.value;
     usersTime = new Date(`${eventMonth.value} ${eventDay.value} ${eventYear.value}`);
     imageSection.style.backgroundImage = `url('${eventImg.value}')`;
+    setTime();
 }
+
+appUpdate();
 
 settingsBtn.addEventListener('click', () => {
     settings.classList.toggle('active')
